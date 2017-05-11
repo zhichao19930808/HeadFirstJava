ffeece4605579715b6b4c9c509342d854e2db4d8
zhichao19930808 <zhichaoglory@163.com>
2017-05-11 14:21:49 +0800

    编写带有main()的类

---
+++ src/main/java/One_EnterTheJavaWorld/MyFirstAPP.java
@@ -0,0 +1,13 @@
+package One_EnterTheJavaWorld;
+
+/**
+ * Created by Administrator on 2017/5/11.
+ * 编写带有main()的类
+ */
+public class MyFirstAPP {
+    //app abbr.应用程序（=application）
+    public static void main(String[] args) {
+        System.out.println("I Rule!");//Rule [ruːl]支配；统治；
+        System.out.println("\tThe World");
+    }
+}
