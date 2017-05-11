1696d4fe1f3874604eb8429e5aee22c22f34fb6b
zhichao19930808 <zhichaoglory@163.com>
2017-05-11 17:07:56 +0800

    条件分支 if test

---
+++ src/main/java/One_EnterTheJavaWorld/IfTest.java
@@ -0,0 +1,15 @@
+package One_EnterTheJavaWorld;
+
+/**
+ * Created by Administrator on 2017/5/11.
+ * 条件分支 if test
+ */
+public class IfTest {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        }
+        System.out.println("this runs no matter what");
+    }
+}
